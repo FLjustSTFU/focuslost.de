e91c237ee4715e40cc72a78009fededa56da1178
FLjustSTFU <levinvonkauffberg@proton.me>
2026-09-07 22:16:25 +0200

    First commit with basic template

---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+//Scene
+const scene = new THREE.Scene();
+
+//Camera
+const camera = new THREE.PerspectiveCamera( 110, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+//Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//Cube
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+//Render loop
+function animate( time ) {
+
+cube.rotation.x = time / 2000;
+cube.rotation.y = time / 1000;
+
+//Render queue
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
